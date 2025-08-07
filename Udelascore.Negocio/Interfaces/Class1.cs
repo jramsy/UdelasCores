51574fb77cf9ea70db0b9d8657d13fbe8e29f7c6
udelas.35620 <udelas.35620@UDELAS35620>
2025-08-07 13:20:42 -0500

    add prueba class

---
+++ Udelascore.Negocio/Interfaces/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udelascore.Negocio.Interfaces
+{
+    internal class Class1
+    {
+    }
+}
